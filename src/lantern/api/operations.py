1acde69b89aaebc34cea80d3e1dcdce7128bf650
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-07-12 17:32:07 +0400

    Подключение к sqlite3

--- src/lantern/api/operations.py
+++ src/lantern/api/operations.py
@@ -14,14 +14,6 @@ router = APIRouter(
     prefix='/operations',
 )
 
-# @router.get('/', response_model=List[Operation])
-# def get_operations(session: Session = Depends(get_session)):
-#     operation = (
-#         session
-#         .query(table.Lantern)
-#         .all()
-#     )
-#     return operation
 
 flashlights = [
     {"id": 1, "status": "Включен", "color": "Белый"},
--- src/lantern/settings.py
+++ src/lantern/settings.py
@@ -4,10 +4,10 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 9999
-    database_url: str = 'sqlite:///./database.sqlite3'
+    database_url: str = 'sqlite:///./flashlights.db'
 
 
 settings = Settings(
-    _env_file='.env',
-    _env_file_encoding='utf-8',
+    # _env_file='.env',
+    # _env_file_encoding='utf-8',
 )
